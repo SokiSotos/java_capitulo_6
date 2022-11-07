478c8e2e7a1ea99c8e3f917e2dcfc59f4f7e0004
SokiSotos <sokisotos@hotmail.com>
2022-11-07 20:17:14 +0100

    Ej09 - Pares 24

--- Ejercicio09.java
+++ Ejercicio09.java
@@ -1,12 +1,13 @@
 public class Ejercicio09 {
     public static void main(String[] args) {
-    int cont = 0;
     int n = 0;
-    while(n!=24){
-        n = ((int)(Math.random()*101));
-        if (n%2==0)
-        cont++;
-    }
-    System.out.println(cont);
+    int cont = 0;
+        while(n!=24){
+            n = ((int)(Math.random()*51)*2);
+            System.out.print(n + " ");
+            cont++;
+        }
+        System.out.println();
+        System.out.printf("El total de numeros generados han sido de %d",cont);
     }
 }
