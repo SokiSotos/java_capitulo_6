a58c6db898de7fac3db2089ea7ca3aabbce75e92
SokiSotos <sokisotos@hotmail.com>
2022-11-07 20:46:43 +0100

    Ej13 - Dados Dobles

---
+++ Ejercicio13.java
@@ -0,0 +1,11 @@
+public class Ejercicio13 {
+    public static void main(String[] args) {
+        int dado1;
+        int dado2;
+        do {
+            dado1 = (int)(Math.random()*6)+1;
+            dado2 = (int)(Math.random()*6)+1;
+                System.out.printf("%d-%d\n",dado1,dado2);
+        }while(dado1!=dado2);
+    }
+}
